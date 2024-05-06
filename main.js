a8d96c91a7ea5d856f4932de6f9af6af7ec6b0bd
Sri Pavani <sripavanikonakanchi21@gmail.com>
2024-05-06 21:01:05 +0530

    changes

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('TODO Application')
+const addBtn = document.getElementById("addissue")
+const form = document.getElementById("formmodle")
+const cancle = document.getElementById("cancel")
+console.log(addBtn,form)
+
+addBtn.addEventListener("click", function (){
+       form.style.display = "block"
+       
+})
+
+cancle.addEventListener("click",function (){
+    form.style.display = "none"
+})
