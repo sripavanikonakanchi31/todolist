50168bfb3dff32c64601118507bc38104f8a6012
Sri Pavani <sripavanikonakanchi21@gmail.com>
2024-05-08 21:00:13 +0530

    changes

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const addBtn = document.getElementById("addissue")
 const form = document.getElementById("formmodle")
 const cancle = document.getElementById("cancel")
-console.log(addBtn,form)
+const titleEle = document.getElementById("title")
+const discEle = document.getElementById("desc")
+const submit = document.getElementById("submit")
+const contEle = document.getElementById("contact")
+const addrEle = document.getElementById("address")
+
 
 addBtn.addEventListener("click", function (){
        form.style.display = "block"
@@ -11,3 +16,28 @@ addBtn.addEventListener("click", function (){
 cancle.addEventListener("click",function (){
     form.style.display = "none"
 })
+
+const todos = [];
+
+submit.addEventListener("click",function (event){
+   event.preventDefault()
+
+   const todo = {
+    title: titleEle.value,
+    desc: discEle.value,
+    contact: contEle.value,
+    address: addrEle.value
+   }
+
+   titleEle.value = ""
+   discEle.value = ""
+   contEle.value = ""
+   addrEle.value = ""
+   todos.push(todo)
+   getTodos()
+})
+
+function getTodos(){
+
+    console.log(todos)
+}
